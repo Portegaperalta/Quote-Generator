eef6e6cb3d7b3c1d8c004d29394b52633e3c38f3
pablothecoder <ortegaperaltap@gmail.com>
2024-11-06 21:59:25 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const newQuoteButton = document.querySelector('button')
-const quote = document.querySelector('.quote')
-const quoteAuthor = document.querySelector('.quote-author')
+const quote = document.querySelector('div .quote p')
+const quoteAuthor = document.querySelector('div .quote-author p')
 
 const quotes = [
   {
@@ -44,3 +44,10 @@ const quotes = [
     author: "Mae West"
   },
 ]
+
+newQuoteButton.addEventListener('click', () => {
+  const random = Math.floor(Math.random() * quotes.length)
+
+  quote.innerText = quotes[random].quote
+  quoteAuthor.innerText = quotes[random].author
+})
